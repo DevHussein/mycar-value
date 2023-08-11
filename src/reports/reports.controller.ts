cc474e768a4fc8933e91ffeefc38ffb0fccf9dd2
“DevHussein” <hussein_989@hotmail.com>
2023-08-11 22:28:27 +0300

    add e2e tests

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from 'src/gurads/auth.guard';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reporstService: ReportsService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  createReport(@Body() body: CreateReportDto) {
+    return this.reporstService.create(body);
+  }
+}
