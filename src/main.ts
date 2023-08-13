d1661648b42d1c9ce72c0d1538e5ca6f902fdd23
“DevHussein” <hussein_989@hotmail.com>
2023-08-13 21:00:50 +0300

    console the app port

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+  console.log(`Our app is running on port ${PORT}`);
 }
 bootstrap();
