d8c4239caf696022f4aed8b10f31910c798996d5
“DevHussein” <hussein_989@hotmail.com>
2023-08-13 22:07:55 +0300

    remove env file for production

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT);
   console.log(`Our app is running on port ${PORT}`);
+  console.log(process.env.NODE_ENV);
 }
 bootstrap();
